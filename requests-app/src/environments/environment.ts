5451f03cd6b5da60de2c1864b48d96e3d52afdce
Ravena <ribeiro.geovana@outlook.com>
2019-11-20 18:56:37 -0300

    Importado firebase e criado model

---
+++ requests-app/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+   firebase: {
+    apiKey: "AIzaSyDFWSuEEznhYEQ302rpO-mJfcc3zS9XmoI",
+    authDomain: "requests-app-c3129.firebaseapp.com",
+    databaseURL: "https://requests-app-c3129.firebaseio.com",
+    projectId: "requests-app-c3129",
+    storageBucket: "requests-app-c3129.appspot.com",
+    messagingSenderId: "605972191946",
+    appId: "1:605972191946:web:2443158a72671ac1e73233"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
